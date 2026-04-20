3c0a163b830f5570930efbbd7867f4145b37502a
39334 <gianluca.pignatelli@students.uniroma2.eu>
2026-04-20 12:28:12 +0200

    final update

--- src/main/java/com/romanimazione/view/Main.java
+++ src/main/java/com/romanimazione/view/Main.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("\n\n************************************************");
-        // Initialize Logic
-        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         Scanner scanner = new Scanner(System.in);
         System.out.println("Select Persistence Mode:");
         System.out.println("1. MySQL");
